b9536f9163c90fce7c36d9e5334965fb2e401734
Anderson Esmaikel Sánchez Donis <sesmaikel@gmail.com>
2024-10-29 21:07:54 -0600

    Actualizacion 4

--- PermisosDeEstudiantes/obj/Debug/net8.0/PermisosDeEstudiantes.AssemblyInfo.cs
+++ PermisosDeEstudiantes/obj/Debug/net8.0/PermisosDeEstudiantes.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermisosDeEstudiantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82174d1d3e7d72505d5c42e0bb434434b63a35bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08106b7ebe23c334479a98330e75302f38086344")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermisosDeEstudiantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermisosDeEstudiantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
